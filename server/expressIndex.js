4d0454837fcbad8f322d67736c17f3c48db00875
ldl <919578094@qq.com>
2021-01-29 13:39:03 +0800

    commit second

--- server/expressIndex.js
+++ server/expressIndex.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 //  主页输出 "Hello World"
-// Make some change
 app.get('/', function (req, res) {
     console.log("主页 GET 请求");
     res.send('Hello GET');
